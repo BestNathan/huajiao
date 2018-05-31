d6a3493c31c5332fd8a13040ed6e811f83a75351
Nathan <308719298@qq.com>
2018-05-31 11:09:39 +0800

    beta-1

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const adm = require('./lib/advertiseManager')
-
-adm
-  .createAdvertiser({
-    roomId: 228209003,
-    contents: '轰炸机测试，如有打扰请见谅。。。',
-    mode: 'bomb',
-    times: 20,
-  })
-  .then(ad => {
-    ad.on('start', roomId => {
-      console.log('advertiser start ar room: ' + roomId)
-    })
-  })
+require('./lib/socket.io')
